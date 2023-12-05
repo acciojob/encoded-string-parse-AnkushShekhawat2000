2e1e11f3e793b1cfd2806135e564d4c1b9611ff8
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 16:03:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+// const parseCode = (str) => {
+//   // your code here
+// };
+
+// // Do not change the code below
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str)));
+
+
 const parseCode = (str) => {
-  // your code here
+  // Split the input string using "000" as the separator
+  const parts = str.split("000");
+
+  // Extract values for firstName, lastName, and id
+  const firstName = parts[0];
+  const lastName = parts[1];
+  const id = parts[2];
+
+  // Return an object with the extracted values
+  return { firstName, lastName, id };
 };
 
-// Do not change the code below
+// Example
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
